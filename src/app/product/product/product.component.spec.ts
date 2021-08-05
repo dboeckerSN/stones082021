847a7d90b4fc6268699821ef29831fdf076a675d
Dominik Böcker <Dominik-Boecker@gmx.de>
2021-08-05 09:47:29 +0200

    Aufgabe 2.2

--- src/app/product/product/product.component.spec.ts
+++ src/app/product/product/product.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Product } from '../product';
 
 import { ProductComponent } from './product.component';
 
@@ -16,11 +18,17 @@ describe('ProductComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+    component.product = new Product(1, 'Gravo', 34, 34);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display name', () => {
+    const name = fixture.debugElement.query(By.css('.nameAttribute'));
+    expect(name.nativeElement.textContent).toContain('Gravo');
   });
 
   it('raisePrice() should raise price', () => {
